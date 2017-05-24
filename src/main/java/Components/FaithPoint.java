8a3ef0872d1352b57c71962de205a0ea33edc67e
SaraPido <sara.pido@mail.polimi.it>
2017-05-24 16:39:16 +0200

    update 24/05

--- src/main/java/Components/FaithPoint.java
+++ src/main/java/Components/FaithPoint.java
@@ -1,5 +1,10 @@
 package Components;
 
+import it.polimi.ingsw.GC_40.Player;
+
 public class FaithPoint extends Point {
+	public void incrementPrivilegeCouncil(Player player){
+		player.incrementFaithPoint(1);
+	}
 
 }
--- src/main/java/Components/Piece.java
+++ src/main/java/Components/Piece.java
@@ -1,5 +1,8 @@
 package Components;
 
+import it.polimi.ingsw.GC_40.Player;
+
 public abstract class Piece {
+	public abstract void incrementPrivilegeCouncil(Player player);
 
 }
