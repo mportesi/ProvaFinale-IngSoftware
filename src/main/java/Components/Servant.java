b79e568348a84af7678fd2c2255d074638f69ba4
SaraPido <sara.pido@mail.polimi.it>
2017-05-24 16:41:56 +0200

    update 24/05

--- src/main/java/Components/Servant.java
+++ src/main/java/Components/Servant.java
@@ -1,5 +1,11 @@
 package Components;
 
+import it.polimi.ingsw.GC_40.Player;
+
 public class Servant extends Resource {
+	
+	public void incrementPrivilegeCouncil(Player player){
+		player.incrementServant(2);
+	}
 
 }
