50ce9efe2747b2f5e5aae33e7958766292d5a78b
SaraPido <sara.pido@mail.polimi.it>
2017-05-29 15:20:54 +0200

    update S

--- src/main/java/it/polimi/ingsw/GC_40/JSon.java
+++ src/main/java/it/polimi/ingsw/GC_40/JSon.java
@@ -41,40 +41,53 @@ import Effects.GainStone;
 import Effects.GainWood;
 
 public class JSon {
-	public static List<Card> buildingDeck = new ArrayList <Card>();
-	public static List<Card> territoryDeck = new ArrayList <Card>();
-	public static List<Card> ventureDeck = new ArrayList <Card>();
-	public static List<Card> characterDeck = new ArrayList <Card>();
-	public static ArrayList<Floor> territoryFloors = new ArrayList <Floor> ();
-	public static ArrayList<Floor> characterFloors = new ArrayList <Floor> ();
-	public static ArrayList<Floor> buildingFloors = new ArrayList <Floor> ();
-	public static ArrayList<Floor> ventureFloors = new ArrayList <Floor> ();
-	
-	public static void importCards() throws FileNotFoundException, IOException, ParseException{
-	
-	
-	JSONParser floorParser = new JSONParser();
-	JSONArray floorArray = (JSONArray) floorParser.parse(new FileReader("C:/Users/Sara/Desktop/Polimi/III anno/II semestre/Ingegneria del software_Gianpaolo Cugola/Workspace/Lab1/ProvaFinale-IngSoftware/Floors.json"));
-	for (Object o : floorArray)
-	{
-		JSONObject floor = (JSONObject) o;
-		
-		String type = (String) floor.get("type");
-		int cost = ((Long) floor.get("cost")).intValue();
-	//	String typeBonus = (String) floor.get("typeBonus");
-		int amountBonus = ((Long) floor.get("amountBonus")).intValue();
-		
-		switch(type){
-		case "territory" :{
-			GainWood e = new GainWood (amountBonus);
-			Floor f = new Floor (type, cost, e);
-			territoryFloors.add(f);
 	public static List<Card> buildingDeck = new ArrayList<Card>();
 	public static List<Card> territoryDeck = new ArrayList<Card>();
 	public static List<Card> ventureDeck = new ArrayList<Card>();
 	public static List<Card> characterDeck = new ArrayList<Card>();
+	public static ArrayList<Floor> territoryFloors = new ArrayList<Floor>();
+	public static ArrayList<Floor> characterFloors = new ArrayList<Floor>();
+	public static ArrayList<Floor> buildingFloors = new ArrayList<Floor>();
+	public static ArrayList<Floor> ventureFloors = new ArrayList<Floor>();
 
 	public static void importCards() throws FileNotFoundException, IOException, ParseException {
+
+		JSONParser floorParser = new JSONParser();
+		JSONArray floorArray = (JSONArray) floorParser.parse(new FileReader("target/BonusFloors.json"));
+		for (Object o : floorArray) {
+			JSONObject floor = (JSONObject) o;
+
+			String type = (String) floor.get("type");
+			int cost = ((Long) floor.get("cost")).intValue();
+			int amountBonus = ((Long) floor.get("amountBonus")).intValue();
+
+			switch (type) {
+			case "territory": {
+				GainWood e = new GainWood(amountBonus);
+				Floor f = new Floor(type, cost, e);
+				territoryFloors.add(f);
+				break;
+			}
+			case "character": {
+				GainStone e = new GainStone(amountBonus);
+				Floor f = new Floor(type, cost, e);
+				characterFloors.add(f);
+				break;
+			}
+			case "building": {
+				GainMilitaryPoint e = new GainMilitaryPoint(amountBonus);
+				Floor f = new Floor(type, cost, e);
+				buildingFloors.add(f);
+				break;
+			}
+			case "venture": {
+				GainCoin e = new GainCoin(amountBonus);
+				Floor f = new Floor(type, cost, e);
+				ventureFloors.add(f);
+				break;
+			}
+			}
+		}
 
 		// BuildingCards
 		JSONParser buildingParser = new JSONParser();
@@ -83,7 +96,7 @@ public class JSon {
 			JSONObject card = (JSONObject) o;
 
 			String type = (String) card.get("type");
-			int period = (int) card.get("period");
+			int period = ((Long) card.get("period")).intValue();
 			String name = (String) card.get("name");
 
 			JSONArray cost = (JSONArray) buildingParser.parse(card.get("cost").toString());
@@ -94,16 +107,15 @@ public class JSon {
 			for (int i = 0; i < cost.size(); i++) {
 				JSONObject costObject = (JSONObject) cost.get(i);
 				String typeCost = (String) costObject.get("type");
-				int amount = (int) costObject.get("amount");
+				int amount = ((Long) costObject.get("amount")).intValue();
 				costMap.put(typeCost, amount);
 			}
-
 
 			Map<String, Integer> immediateEffectMap = new LinkedHashMap();
 			for (int i = 0; i < immediateEffect.size(); i++) {
 				JSONObject immediateEffectObject = (JSONObject) immediateEffect.get(i);
 				String typeImmediateEffect = (String) immediateEffectObject.get("type");
-				int amount = (int) immediateEffectObject.get("amount");
+				int amount = ((Long) immediateEffectObject.get("amount")).intValue();
 				immediateEffectMap.put(typeImmediateEffect, amount);
 			}
 
@@ -111,95 +123,29 @@ public class JSon {
 
 			buildingDeck.add(c);
 		}
-		case "character" :{
-			GainStone e = new GainStone (amountBonus);
-			Floor f = new Floor (type, cost, e);
-			characterFloors.add(f);
-		}
-		case "building" :{
-			GainMilitaryPoint e = new GainMilitaryPoint (amountBonus);
-			Floor f = new Floor (type, cost, e);
-			buildingFloors.add(f);
-		}
-		case "venture" :{
-			GainCoin e = new GainCoin (amountBonus);
-			Floor f = new Floor (type, cost, e);
-			ventureFloors.add(f);
-		}
-	}
-	
-	
-	}
-	
-	//BuildingCards
-	JSONParser buildingParser = new JSONParser();
-	JSONArray buildingArray = (JSONArray) buildingParser.parse(new FileReader("C:/Users/Sara/Desktop/Polimi/III anno/II semestre/Ingegneria del software_Gianpaolo Cugola/Workspace/Lab1/ProvaFinale-IngSoftware/Carte Torri.json"));
-	  for (Object o : buildingArray)
-	  {
-	    JSONObject card = (JSONObject) o;
-	 
-	    
-	    String type = (String) card.get("type");
-	    int period = ((Long) card.get("period")).intValue();
-	    String name = (String) card.get("name");
 
-	    
+		// TerritoryCards
+		JSONParser territoryParser = new JSONParser();
+		JSONArray territoryArray = (JSONArray) territoryParser.parse(new FileReader("target/TerritoryCards.json"));
+		for (Object o : territoryArray) {
+			JSONObject card = (JSONObject) o;
 
-	
+			String type = (String) card.get("type");
+			int period = ((Long) card.get("period")).intValue();
+			String name = (String) card.get("name");
 
-	    Map<String, Integer> costMap = new LinkedHashMap();
-	    for (int i = 0; i < cost.size(); i++) {  
-	    	JSONObject costObject = (JSONObject) cost.get(i);
-	        String typeCost = (String) costObject.get("type");
-	        int amount = ((Long) costObject.get("amount")).intValue();
-	        costMap.put(typeCost, amount);
-	    }
-	    
-	    /*List <String> cost = new ArrayList<String>();
-        for (String key: cost1.keySet()){
-        	System.out.println( key );
-        	
-        	keys.add(key);
-        	
-        } va in card*/
-	   
-	    Map<String, Integer> immediateEffectMap = new LinkedHashMap();
-	    for (int i = 0; i < immediateEffect.size(); i++) {  
-	    	JSONObject immediateEffectObject = (JSONObject) immediateEffect.get(i);
-	        String typeImmediateEffect = (String) immediateEffectObject.get("type");
-	        int amount = ((Long) immediateEffectObject.get("amount")).intValue();
-	        immediateEffectMap.put(typeImmediateEffect, amount);
-	    }
-	    
-	    Card c= new BuildingCard(type, name, period, costMap, immediateEffectMap);
-	   
-		buildingDeck.add(c);
-	  }
-	 
-	 //TerritoryCards
-	  JSONParser territoryParser = new JSONParser();
-		JSONArray territoryArray = (JSONArray) territoryParser.parse(new FileReader("C:/Users/Sara/Desktop/Polimi/III anno/II semestre/Ingegneria del software_Gianpaolo Cugola/Workspace/Lab1/ProvaFinale-IngSoftware/Carte Torri.json"));
-		  for (Object o : territoryArray)
-		  {
-		    JSONObject card = (JSONObject) o;
-		 
-		    
-		    String type = (String) card.get("type");
-		    int period = ((Long) card.get("period")).intValue();
-		    String name = (String) card.get("name");
-		    
-		    JSONArray immediateEffect=(JSONArray) territoryParser.parse (card.get("immediateEffect").toString());
-		    
-		    Map<String, Integer> immediateEffectMap = new LinkedHashMap();
-		    for (int i = 0; i < immediateEffect.size(); i++) {  
-		    	JSONObject immediateEffectObject = (JSONObject) immediateEffect.get(i);
-		        String typeImmediateEffect = (String) immediateEffectObject.get("type");
-		        int amount = ((Long) immediateEffectObject.get("amount")).intValue();
-		        immediateEffectMap.put(typeImmediateEffect, amount);
-		    }
-		    
-		    Card c= new TerritoryCard(type, name, period, immediateEffectMap);
-		   
+			JSONArray immediateEffect = (JSONArray) territoryParser.parse(card.get("immediateEffect").toString());
+
+			Map<String, Integer> immediateEffectMap = new LinkedHashMap();
+			for (int i = 0; i < immediateEffect.size(); i++) {
+				JSONObject immediateEffectObject = (JSONObject) immediateEffect.get(i);
+				String typeImmediateEffect = (String) immediateEffectObject.get("type");
+				int amount = ((Long) immediateEffectObject.get("amount")).intValue();
+				immediateEffectMap.put(typeImmediateEffect, amount);
+			}
+
+			Card c = new TerritoryCard(type, name, period, immediateEffectMap);
+
 			territoryDeck.add(c);
 		}
 
@@ -210,11 +156,11 @@ public class JSon {
 			JSONObject card = (JSONObject) o;
 
 			String type = (String) card.get("type");
-			int period = (int) card.get("period");
+			int period = ((Long) card.get("period")).intValue();
 			String name = (String) card.get("name");
-			int alternativeCostBoolean = (int) card.get("alternativeCostBoolean");
-			int militaryRequirement = (int) card.get("militaryRequirement");
-			int militaryCost = (int) card.get("militaryCost");
+			int alternativeCostBoolean = ((Long) card.get("alternativeCostBoolean")).intValue();
+			int militaryRequirement = ((Long) card.get("militaryRequirement")).intValue();
+			int militaryCost = ((Long) card.get("militaryCost")).intValue();
 			JSONArray cost = (JSONArray) ventureParser.parse(card.get("cost").toString());
 
 			JSONArray immediateEffect = (JSONArray) ventureParser.parse(card.get("immediateEffect").toString());
@@ -223,7 +169,7 @@ public class JSon {
 			for (int i = 0; i < cost.size(); i++) {
 				JSONObject costObject = (JSONObject) cost.get(i);
 				String typeCost = (String) costObject.get("type");
-				int amount = (int) costObject.get("amount");
+				int amount = ((Long) costObject.get("amount")).intValue();
 				costMap.put(typeCost, amount);
 			}
 
@@ -231,7 +177,7 @@ public class JSon {
 			for (int i = 0; i < immediateEffect.size(); i++) {
 				JSONObject immediateEffectObject = (JSONObject) immediateEffect.get(i);
 				String typeImmediateEffect = (String) immediateEffectObject.get("type");
-				int amount = (int) immediateEffectObject.get("amount");
+				int amount = ((Long) immediateEffectObject.get("amount")).intValue();
 				immediateEffectMap.put(typeImmediateEffect, amount);
 			}
 			Card c;
@@ -253,30 +199,30 @@ public class JSon {
 			JSONObject card = (JSONObject) o;
 
 			String type = (String) card.get("type");
-			int period = (int) card.get("period");
+			int period = ((Long) card.get("period")).intValue();
 			String name = (String) card.get("name");
-			int costCoin = (int) card.get("costCoin");
+			int costCoin = ((Long) card.get("costCoin")).intValue();
 			String bonusCard = (String) card.get("getCard");
 
-			JSONArray immediateEffect = (JSONArray) buildingParser.parse(card.get("immediateEffect").toString());
+			JSONArray immediateEffect = (JSONArray) characterParser.parse(card.get("immediateEffect").toString());
 
 			Map<String, Integer> immediateEffectMap = new LinkedHashMap();
 			for (int i = 0; i < immediateEffect.size(); i++) {
 				JSONObject immediateEffectObject = (JSONObject) immediateEffect.get(i);
 				String typeImmediateEffect = (String) immediateEffectObject.get("type");
-				int amount = (int) immediateEffectObject.get("amount");
+				int amount = ((Long) immediateEffectObject.get("amount")).intValue();
 				immediateEffectMap.put(typeImmediateEffect, amount);
 			}
 
 			if (bonusCard != null) {
 				int valueGetCard = (int) card.get("valueGetCard");
-				JSONArray discount = (JSONArray) buildingParser.parse(card.get("discount").toString());
+				JSONArray discount = (JSONArray) characterParser.parse(card.get("discount").toString());
 
 				Map<String, Integer> discountMap = new LinkedHashMap();
 				for (int i = 0; i < discount.size(); i++) {
 					JSONObject discountObject = (JSONObject) discount.get(i);
 					String typeDiscount = (String) discountObject.get("typeDiscount");
-					int amount = (int) discountObject.get("amount");
+					int amount = ((Long) discountObject.get("amount")).intValue();
 					discountMap.put(typeDiscount, amount);
 				}
 
@@ -289,81 +235,7 @@ public class JSon {
 				characterDeck.add(c);
 			}
 
-			 /*
-			    
-			    String type = (String) card.get("type");
-			    int period = ((Long) card.get("period")).intValue();
-			    String name = (String) card.get("name");
-			    int alternativeCostBoolean= ((Long) card.get("alternativeCostBoolean")).intValue();
-			    int militaryRequirement=((Long) card.get("militaryRequirement")).intValue();
-			    int militaryCost=((Long) card.get("militaryCost")).intValue();
-			    JSONArray cost= (JSONArray) ventureParser.parse (card.get("cost").toString());
-			    
-			    JSONArray immediateEffect=(JSONArray) ventureParser.parse (card.get("immediateEffect").toString());
-			    
+		}
 
-			    Map<String, Integer> costMap = new LinkedHashMap();
-			    for (int i = 0; i < cost.size(); i++) {  
-			    	JSONObject costObject = (JSONObject) cost.get(i);
-			        String typeCost = (String) costObject.get("type");
-			        int amount = ((Long) costObject.get("amount")).intValue();
-			        costMap.put(typeCost, amount);
-			    }
-			    
-			    Map<String, Integer> immediateEffectMap = new LinkedHashMap();
-			    for (int i = 0; i < immediateEffect.size(); i++) {  
-			    	JSONObject immediateEffectObject = (JSONObject) immediateEffect.get(i);
-			        String typeImmediateEffect = (String) immediateEffectObject.get("type");
-			        int amount =((Long) immediateEffectObject.get("amount")).intValue();
-			        immediateEffectMap.put(typeImmediateEffect, amount);
-			    }
-			    Card c;
-			    if(alternativeCostBoolean==1){
-			    	 c= new VentureCard(type, name, period, costMap, militaryRequirement, militaryCost, immediateEffectMap);
-			    }else if(militaryRequirement==0 && militaryCost==0){
-			    	c= new VentureCard(type, name, period, costMap, immediateEffectMap);
-			    }else {c=new VentureCard(type, name, period, militaryRequirement, militaryCost, costMap, immediateEffectMap);}
-			    
-			   
-				ventureDeck.add(c);
-			  }*/
-			  
-		  //CharacterCard
-			
-			  JSONParser characterParser = new JSONParser();
-				JSONArray characterArray = (JSONArray) characterParser.parse(new FileReader("C:/Users/Sara/Desktop/Polimi/III anno/II semestre/Ingegneria del software_Gianpaolo Cugola/Workspace/Lab1/ProvaFinale-IngSoftware/Carte Torri.json"));
-				  for (Object o : characterArray)
-				  {
-				    JSONObject card = (JSONObject) o;
-				 
-				    
-				    String type = (String) card.get("type");
-				    int period = ((Long) card.get("period")).intValue();
-				    String name = (String) card.get("name");
-				    int costCoin= ((Long) card.get("costCoin")).intValue();
-				  
-				    
-				    JSONArray immediateEffect=(JSONArray) buildingParser.parse (card.get("immediateEffect").toString());
-				    
-				    Map<String, Integer> immediateEffectMap = new LinkedHashMap();
-				    for (int i = 0; i < immediateEffect.size(); i++) {  
-				    	JSONObject immediateEffectObject = (JSONObject) immediateEffect.get(i);
-				        String typeImmediateEffect = (String) immediateEffectObject.get("type");
-				        int amount = ((Long) immediateEffectObject.get("amount")).intValue();
-				        immediateEffectMap.put(typeImmediateEffect, amount);
-				    }
-				    
-				    Card c= new CharacterCard(type, name, period, costCoin,  immediateEffectMap);
-				   
-					characterDeck.add(c);
-				  }
-				 
-		 
-	  
-	  
-	  
-	
-	
 	}
-
 }
