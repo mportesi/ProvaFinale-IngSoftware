ba5438faa5017284eff9e4de71edd2fe75a6f2f9
Unknown <chiara.plizzari@mail.polimi.it>
2017-06-02 20:12:21 +0200

    update c

--- src/main/java/it/polimi/ingsw/effects/GainPrivilegeCouncil.java
+++ src/main/java/it/polimi/ingsw/effects/GainPrivilegeCouncil.java
@@ -5,18 +5,19 @@ import it.polimi.ingsw.components.PrivilegeCouncil;
 
 public class GainPrivilegeCouncil extends Effect {
 	int privilegeCouncil;
+	String resource;
 	
-	public GainPrivilegeCouncil(int privilegeCouncil){
+	public GainPrivilegeCouncil(int privilegeCouncil, String resource){
 		this.privilegeCouncil=privilegeCouncil;
-		
+		this.resource = resource;
 	}
 
 	@Override
 	public void apply(Player player) {
-		String resource; // =controller chooseResource(se privilegeCouncil>1 sceglie due cose diverse);
+		// Resource resource = chooseResource();
 		PrivilegeCouncil.applyEffect(player, resource);
 		
-	
+	//metto uno switch e incremento a seconda del case
 		
 		
 
