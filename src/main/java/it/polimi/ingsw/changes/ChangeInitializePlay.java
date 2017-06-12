77e7b05c2e00947d93371b6d0925fb4eba23f2ec
SaraPido <sara.pido@mail.polimi.it>
2017-06-12 17:02:35 +0200

    update S

--- src/main/java/it/polimi/ingsw/changes/ChangeInitializePlay.java
+++ src/main/java/it/polimi/ingsw/changes/ChangeInitializePlay.java
@@ -1,14 +1,17 @@
 package it.polimi.ingsw.changes;
 
+import java.util.ArrayList;
+
 import it.polimi.ingsw.GC_40.Player;
 import it.polimi.ingsw.client.ClientModel;
 
 public class ChangeInitializePlay implements Change {
 	private int numberOfPlayers;
-	
+	private ArrayList<Player> players;
 	
 	public ChangeInitializePlay(int numberOfPlayers){
 		this.numberOfPlayers=numberOfPlayers;
+		this.players=players;
 		
 	}
 
@@ -16,7 +19,6 @@ public class ChangeInitializePlay implements Change {
 	public void applyChange(ClientModel client) {
 		//System.out.println("The game can start");
 		client.setStartPlay(true);
-
 	}
 
 }
