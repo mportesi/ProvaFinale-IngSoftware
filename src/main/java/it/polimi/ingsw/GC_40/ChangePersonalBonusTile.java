deffadcbf7137b627060cd56e17e321e94fb7415
matteo <portesimatt@gmail.com>
2017-06-15 20:55:08 +0200

    updt M

--- src/main/java/it/polimi/ingsw/GC_40/ChangePersonalBonusTile.java
+++ src/main/java/it/polimi/ingsw/GC_40/ChangePersonalBonusTile.java
@@ -1,6 +1,21 @@
 package it.polimi.ingsw.GC_40;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.json.simple.parser.ParseException;
+
+import it.polimi.ingsw.changes.Change;
+import it.polimi.ingsw.client.ClientModel;
+
 public class ChangePersonalBonusTile implements Change{
+
+	@Override
+	public void applyChange(ClientModel client)
+			throws FileNotFoundException, NullPointerException, IOException, ParseException, InterruptedException {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 }
