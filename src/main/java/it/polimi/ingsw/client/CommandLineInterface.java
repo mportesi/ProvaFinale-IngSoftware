2d8a6bfb6e5a3145f853b157ed87ff69ae2434ca
matteo <portesimatt@gmail.com>
2017-06-16 15:43:36 +0200

    update cli with loop M

--- src/main/java/it/polimi/ingsw/client/CommandLineInterface.java
+++ src/main/java/it/polimi/ingsw/client/CommandLineInterface.java
@@ -47,7 +47,8 @@ public class CommandLineInterface implements Serializable {
 		System.out.println("\n Choose what relative you want to use: \n 1)  black \n 2) white \n 3) orange \n 4) neutral");
 		Relative relative = null;
 		int input=0;
-		try{ input = scanner.nextInt();
+		while (!scanner.hasNextInt()) scanner.next();
+		/*try{*/ input = scanner.nextInt();
 		switch (input) {
 		case 1: {
 			if (client.getPlayer().getBooleanRelative(client.getPlayer().getBlackRelative())) {
@@ -99,12 +100,12 @@ public class CommandLineInterface implements Serializable {
 			break;
 		}
 		}
-		}
+		/*}
 		catch(InputMismatchException e){
 			System.out.println("\nError: insert again");
 			relative= chooseTheRelative();
 			return relative;
-		}
+		}*/
 		
 		
 		
@@ -138,7 +139,8 @@ public class CommandLineInterface implements Serializable {
 		
 
 		PutRelative putRelative = null;
-		try{int input = scanner.nextInt();
+		while (!scanner.hasNextInt()) scanner.next();
+		/*try{*/int input = scanner.nextInt();
 			switch (input) {
 		case 1: {
 			Tower tower = chooseTower();
@@ -186,12 +188,17 @@ public class CommandLineInterface implements Serializable {
 
 			break;
 		}
-}}
+		default:{
+			System.out.println("Try again");
+			putRelative=chooseTheAction(relative);
+		}
+		}
+		/*}
 		catch(InputMismatchException e){
 			System.out.println("\nError: insert again");
 			putRelative = chooseTheAction(relative);
 			
-		}
+		}*/
 
 		return putRelative;
 	}
@@ -242,8 +249,10 @@ public class CommandLineInterface implements Serializable {
 	private boolean chooseAlternativeCost() {
 		boolean choice = false;
 		System.out.println("\nChoose if you prefer: \n 1)military cost \n 2)the other cost");
-		try{
-		switch (scanner.nextInt()) {
+		/*try{*/
+		while (!scanner.hasNextInt()) scanner.next();
+		int input= scanner.nextInt();
+		switch (input) {
 		case 1: {
 			choice = true;
 			return choice;
@@ -256,12 +265,13 @@ public class CommandLineInterface implements Serializable {
 			System.out.println("\nError: insert again");
 			choice = chooseAlternativeCost();
 		}
-		}}
+		}
+		/*}
 		catch(InputMismatchException e){
 			System.out.println("\nError: insert again");
 			choice = chooseAlternativeCost();
 			
-		}
+		}*/
 		return choice;
 	}
 
@@ -272,9 +282,10 @@ public class CommandLineInterface implements Serializable {
 		System.out.println("2) Building tower");
 		System.out.println("3) Character tower");
 		System.out.println("4) Venture tower");
+		while (!scanner.hasNextInt()) scanner.next();
 		int input = scanner.nextInt();
 		Tower tower;
-		try{
+		/*try{*/
 		switch (input) {
 		case 1: {
 			tower = this.client.getTerritoryTower();
@@ -296,28 +307,31 @@ public class CommandLineInterface implements Serializable {
 			System.out.println("\nError: insert again");
 			tower = chooseTower();
 			return tower;
-		}}}
+		}}
+		/*}
 		catch(InputMismatchException e){
 			System.out.println("\nError: insert again");
 			tower = chooseTower();
 			
-		}
-		return null;
+		}*/
+		//return null;
 	}
 
 	public int chooseFloor() {
 		System.out.println("\nChoose the number of the floor:");
 		int floor;
-		try{floor = scanner.nextInt();
+		while (!scanner.hasNextInt()) scanner.next();
+		/*try{*/floor = scanner.nextInt();
 		floor -= 1;
 		if (floor < 0 || floor > 4) {
 			System.out.println("\nThat floor don't exist!");
 			floor = chooseFloor();
-		}}
+		}
+		/*}
 		catch(InputMismatchException e){
 			System.out.println("\nError: insert again");
 			floor = chooseFloor();
-		}
+		}*/
 		
 		return (floor);
 	}
@@ -332,11 +346,12 @@ public class CommandLineInterface implements Serializable {
 		System.out.println("5) 2 militaryPoint");
 		String resource=null;
 		int choice=0;
-		try{choice = scanner.nextInt();}
-		catch(InputMismatchException e){
+		while (!scanner.hasNextInt()) scanner.next();
+		/*try{*/choice = scanner.nextInt();//}
+		/*catch(InputMismatchException e){
 			System.out.println("\nError: insert again");
 			choosePrivilegeCouncil();
-		}
+		}*/
 		switch(choice){
 		case 1:{ resource="coin";
 				break;
@@ -377,11 +392,12 @@ public class CommandLineInterface implements Serializable {
 			}
 			// Choose if you want to place the relative left or right
 			System.out.println("\nChoose where you want to put your relative: \n1) left \n2) right");
-			try{choice=scanner.nextInt();}
-			catch(InputMismatchException e){
+			while (!scanner.hasNextInt()) scanner.next();
+			/*try{*/choice=scanner.nextInt();//}
+			/*catch(InputMismatchException e){
 				System.out.println("\nError: insert again");
 				chooseHarvestArea();
-			}
+			}*/
 			if (choice < 1 || choice > 2) {
 				System.out.println("\nError: insert again");
 				chooseHarvestArea();
@@ -412,11 +428,12 @@ public class CommandLineInterface implements Serializable {
 			}
 		// Choose if you want to place the relative left or right
 		System.out.println("\nChoose where you want to put your relative: \n1) left \n2) right");
-		try{choice=scanner.nextInt();}
-		catch(InputMismatchException e){
+		while (!scanner.hasNextInt()) scanner.next();
+		/*try{*/choice=scanner.nextInt();//}
+		/*catch(InputMismatchException e){
 			System.out.println("\nError: insert again");
 			chooseProductionArea();
-		}
+		}*/
 		if (choice < 1 || choice > 2) {
 			System.out.println("\nError: insert again");
 			chooseProductionArea();
