c3aa7e60174fd0f28d26e70a750e2f54632d5803
SaraPido <sara.pido@mail.polimi.it>
2017-06-17 12:31:06 +0200

    timeout actions

--- src/main/java/it/polimi/ingsw/clientRMI/ClientRMIConnection.java
+++ src/main/java/it/polimi/ingsw/clientRMI/ClientRMIConnection.java
@@ -10,6 +10,8 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.json.simple.parser.ParseException;
 
@@ -52,39 +54,52 @@ public class ClientRMIConnection implements Serializable {
 		// register the client view in the server side (to receive messages from
 		// the server)
 
-		
 		ClientRMIConnectionView rmiView = new ClientRMIConnectionView(clientModel);
 
 		System.out.println("\nTell me your name\n");
 		String name = stdIn.nextLine();
 		clientModel.setName(name);
-	
+
 		// register the client view in the server side (to receive messages from
 		// the server)
 		serverStub.registerClient(rmiView, name);
-		
-		System.out.println("\nPress a key if you want to play otherwise write 'quit'");
-		
-		while (stdIn.nextLine()!="quit") {
-			if(clientModel.getCurrentPlayer()!=null){
-				
+
+		while (true) {
+			if (clientModel.getCurrentPlayer() != null) {
+				float timeoutActions = (float) (System.currentTimeMillis() + 50 * 10000);
 				while (clientModel.getCurrentPlayer().getName().equals(clientModel.getPlayer().getName())) {
 					System.out.println("\nIt's the " + clientModel.getCurrentPlayer().getName() + "'s turn.");
+					Timer timer = new Timer();
+					timer.schedule(new TimerTask() {
+						@Override
+						public void run() {
+							System.out.println("It ran out of time!");
+							ShiftPlayer shiftPlayer = new ShiftPlayer();
+							try {
+								serverStub.notifyObserver(shiftPlayer);
+							} catch (NullPointerException | IOException | ParseException | InterruptedException e) {
+								e.printStackTrace();
+							}
+						}
+					}, 20 * 10000); // TODO IMPORTARE DA JSON }}
 					// Capture input from user
 					CommandLineInterface commandLineInterface = new CommandLineInterface(clientModel);
 					System.out.println("\nPress a key to start the action");
 					String inputLine = stdIn.nextLine();
-					Relative relative=commandLineInterface.chooseTheRelative();
-					int servant=commandLineInterface.chooseServants(relative);
+					Relative relative = commandLineInterface.chooseTheRelative();
+					int servant = commandLineInterface.chooseServants(relative);
 					serverStub.notifyObserver(new SetServant(servant, clientModel.getPlayer(), relative));
 					PutRelative putRelative = commandLineInterface.chooseTheAction(relative);
 					serverStub.notifyObserver(putRelative);
 					System.out.println("\nNow your personal board is: \n" + clientModel.getPlayer());
+				}
 
-			}}
+	
+
+			}
+			// otherwise it is slow
+			Thread.sleep(10 * 100);
+
 		}
-
 	}
-
-
 }
