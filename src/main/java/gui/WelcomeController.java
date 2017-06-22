d36f0a7c80f289becf3691feed33be5a26bd03a3
SaraPido <sara.pido@mail.polimi.it>
2017-06-22 16:18:08 +0200

    update S

--- src/main/java/gui/WelcomeController.java
+++ src/main/java/gui/WelcomeController.java
@@ -1,11 +1,19 @@
 package gui;
 
+import java.io.IOException;
+import java.nio.channels.AlreadyBoundException;
+import java.rmi.NotBoundException;
+
+import org.json.simple.parser.ParseException;
+
 import it.polimi.ingsw.clientRMI.ClientRMIConnection;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class WelcomeController {
+	
 	@FXML
 	private Label welcome;
 	@FXML
@@ -15,11 +23,26 @@ public class WelcomeController {
 	@FXML
 	private Button rmi;
 	
-	
+	private Gui gui;
 	private ClientRMIConnection client;
+	private String host = "127.0.0.1";
+	private int rmi_port = 52365;
 	
-	public WelcomeController(int rmi_port, String host){
+	/*public WelcomeController(int rmi_port, String host){
 		client = new ClientRMIConnection(rmi_port, host);
+	}*/
+	
+	@FXML
+	public void rmi(){
+		try {
+			client = new ClientRMIConnection(rmi_port, host);
+			client.startClient();
+			gui.register();
+		} catch (AlreadyBoundException | NullPointerException | NotBoundException | IOException | ParseException
+				| InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	
