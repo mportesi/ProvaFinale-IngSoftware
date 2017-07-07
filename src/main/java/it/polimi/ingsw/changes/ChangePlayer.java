66ae446e751cb0ee0be985e4f1993935bc9507c6
SaraPido <sara.pido@mail.polimi.it>
2017-07-07 22:09:30 +0200

    update S

--- src/main/java/it/polimi/ingsw/changes/ChangePlayer.java
+++ src/main/java/it/polimi/ingsw/changes/ChangePlayer.java
@@ -15,8 +15,8 @@ public class ChangePlayer implements Change {
 		if (currentPlayer.getMatch() == client.getPlayer().getMatch()){
 		client.getTimer().cancel();
 		client.setCurrentPlayer(currentPlayer);
+		System.out.println(client.getPlayer());
 		System.out.println("It's the " + currentPlayer.getName() + "'s turn.");
-		
 	}
 	}
 	
