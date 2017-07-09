19a0e272d367ade1085e215d1cb1e403971700fa
matteo <portesimatt@gmail.com>
2017-07-10 00:29:26 +0200

    up test

--- src/test/java/it/polimi/ingsw/areas/MarketBuildingTest.java
+++ src/test/java/it/polimi/ingsw/areas/MarketBuildingTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 
 import it.polimi.ingsw.GC_40.Play;
 import it.polimi.ingsw.GC_40.Player;
+import it.polimi.ingsw.colors.ColorDice;
 import it.polimi.ingsw.components.Relative;
 import it.polimi.ingsw.effects.GainCoin;
 
@@ -87,18 +88,39 @@ public class MarketBuildingTest {
 	}
 
 	@Test
-	public void testSetPlayer() {
-		fail("Not yet implemented");
+	public void testSetPlayer() throws FileNotFoundException, IOException, ParseException {
+		Map <String, Integer> bonusMap=new LinkedHashMap();
+		bonusMap.put("coin", 1);
+		MarketListOfEffect effect=new MarketListOfEffect(bonusMap);
+		MarketBuilding market=new MarketBuilding("market1", effect, 0);
+		Relative relative = new Relative(null, null);
+		Player player = new Player(null, null, "testName", 0);
+		market.setPlayer(player);
+		assertEquals("testName",market.getPlayer().getName());
 	}
 
 	@Test
-	public void testGetPlayer() {
-		fail("Not yet implemented");
+	public void testGetPlayer() throws FileNotFoundException, IOException, ParseException {
+		Map <String, Integer> bonusMap=new LinkedHashMap();
+		bonusMap.put("coin", 1);
+		MarketListOfEffect effect=new MarketListOfEffect(bonusMap);
+		MarketBuilding market=new MarketBuilding("market1", effect, 0);
+		Relative relative = new Relative(null, null);
+		Player player = new Player(null, null, "testName", 0);
+		market.setPlayer(player);
+		assertEquals("testName",market.getPlayer().getName());
 	}
 
 	@Test
-	public void testGetRelative() {
-		fail("Not yet implemented");
+	public void testGetRelative() throws FileNotFoundException, IOException, ParseException, NullPointerException, InterruptedException {
+		Map <String, Integer> bonusMap=new LinkedHashMap();
+		bonusMap.put("coin", 1);
+		MarketListOfEffect effect=new MarketListOfEffect(bonusMap);
+		MarketBuilding market=new MarketBuilding("market1", effect, 0);
+		Relative relative = new Relative(ColorDice.BLACK, null);
+		Player player = new Player(null, null, null, 0);
+		market.setOccupied(player, relative, market);
+		assertEquals(ColorDice.BLACK, market.getRelative().getColor());
 	}
 
 }
